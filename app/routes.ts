08680c8e89e06f227dbbe8e18fffd4b5d867c570
mahiro <cokawa.mahiro@gmail.com>
2025-06-29 16:38:28 +0700

    chore: update routes structure and tailwindcss

--- app/routes.ts
+++ app/routes.ts
@@ -1,28 +1,17 @@
-import { index, type RouteConfig } from '@react-router/dev/routes'
+import { index, layout, type RouteConfig, route } from '@react-router/dev/routes'
 
 export default [
-  // Index route for the home page
-  index('routes/home.tsx'),
+  layout('routes/landing/layout.tsx', [
+    // Index route for the home page
+    index('routes/landing/index.tsx'),
 
-  // About page
-  // route('about', 'routes/about.tsx'),
+    // About page
+    route('about', 'routes/landing/about.tsx'),
 
-  // // Docs section with nested routes
-  // ...prefix('docs', [
-  //   layout('routes/docs/layout.tsx', [
-  //     index('routes/docs/index.tsx'),
-  //     route('getting-started', 'routes/docs/getting-started.tsx'),
-  //     route('advanced', 'routes/docs/advanced.tsx'),
-  //   ]),
-  // ]),
+    // Contact page
+    route('contact', 'routes/landing/contact.tsx'),
 
-  // // Projects section with dynamic segments
-  // ...prefix('projects', [
-  //   index('routes/projects/index.tsx'),
-  //   layout('routes/projects/layout.tsx', [
-  //     route(':projectId', 'routes/projects/project.tsx'),
-  //     route(':projectId/edit', 'routes/projects/edit.tsx'),
-  //     route(':projectId/settings', 'routes/projects/settings.tsx'),
-  //   ]),
-  // ]),
+    // Pricing page
+    route('pricing', 'routes/landing/pricing.tsx'),
+  ]),
 ] satisfies RouteConfig
